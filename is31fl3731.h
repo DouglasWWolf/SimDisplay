c14e2afae7367fa94b7e6e2fd3afac90054bd712
DouglasWWolf <douglaswolf@yahoo.com>
2021-11-29 17:24:01 -0800

    See History.h for changes

--- is31fl3731.h
+++ is31fl3731.h
@@ -11,42 +11,42 @@ class IS31FL3731
 public:
 
     // Call this once at program startup to initialize the device
-    void	init(int i2c_address, unsigned char brightness = 255);
+    void    init(int i2c_address, unsigned char brightness = 255);
 
     // Call this to set the brightness of an "on" LED
-    void	set_brightness(unsigned char brightness, bool update_display = false);
+    void    set_brightness(unsigned char brightness, bool update_display = false);
 
     // Call this to determine the display orientation.  0 = Normal, 1 = Upside down
-    void	set_orientation(int orientation) { m_orientation = orientation; }
+    void    set_orientation(int orientation) { m_orientation = orientation; }
 
     // Call this to draw a vertical bar in a given column
     void    vertical_bar(int column, bool update_display = true);
 
     // Call this to print a pair of characters
-    void	print(char c1, char c2);
+    void    print(char c1, char c2);
 
     // Call this to print a two digit integer
-    void	print(int);
+    void    print(int);
 
     // Call this to print a rounded two-digit floating point number
-    void	print(double);
+    void    print(double);
 
     // Call this to print a 2-character string
-    void	print(const char*);
+    void    print(const char*);
 
 protected:
 
     // This is a generic routine that transmits to the device via I2C
-    void	transmit(const unsigned char* data, size_t length);
+    void    transmit(const unsigned char* data, size_t length);
 
     // This updates a single 8-bit register on the device
-    void	write_reg(unsigned char address, unsigned char value);
+    void    write_reg(unsigned char address, unsigned char value);
 
     // Sends the bitmap to the device
-    void	display_image();
+    void    display_image();
 
     // Displays a single character
-    void	print(int row, int col, uint8_t c);
+    void    print(int row, int col, uint8_t c);
 
     // The number of columns and rows that our bitmap and the device can handle
     enum { MAX_COLS = 16, MAX_ROWS = 9};
@@ -61,7 +61,7 @@ protected:
     unsigned char m_brightness;
 
     // Display orientation: 0 = normal, 1 = flip it upside down
-    int		m_orientation;
+    int m_orientation;
 
     // The I2C address of the device we're controlling
     unsigned char m_i2c_address;
